694a42c86cac669214c8f4c7af5a39e97c5ff77a
Prithesh Dwivedi <66085589+JUNKIEJAM@users.noreply.github.com>
2023-11-26 20:03:03 +0530

    Building Buttons (Partial)

---
+++ src/Components/sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import "./Sidebar.scss";
+
+const Sidebar = () => {
+    return 
+    <div className="sidebar">
+        <div className="bg">
+            <Links />
+        </div>
+        <ToggleButton />
+    </div>
+};
+
+export default Sidebar;
