a7a0485b4ed04fd86c3bc462c3c339652f346834
Prithesh Dwivedi <66085589+JUNKIEJAM@users.noreply.github.com>
2023-11-26 18:55:15 +0530

    Introducing Framer Motion Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import Navbar from './Components/navbar/navbar';
+import Test from './Test';
 import "./app.scss";
 
 const App = () => {
   return <div>
 
-    <section><a href ="">Hello</a></section>
+    {/* <section>
+      <Navbar />
+    </section>
     <section>Parallax</section>
     <section>Services</section>
     <section>Parallax</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section>Contact</section>
+    <section>Contact</section> */}
+    <Test></Test>
     </div>;
 };
 
